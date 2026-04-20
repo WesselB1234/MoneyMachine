4bbeec343b31b69166b894d2c73433ce2bfb0281
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-04-20 13:18:23 +0200

    Project setup v2

---
+++ MoneyMachine/MoneyMachine/src/test/java/MoneyMachine/MoneyMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package MoneyMachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
