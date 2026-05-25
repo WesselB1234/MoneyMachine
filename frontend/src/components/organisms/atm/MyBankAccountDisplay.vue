c9b1d3f716f7abfd3c88cb5265c864a93c0eb781
WesselB1234 <stosin007@hotmail.com>
2026-05-25 22:36:02 +0200

    Feat: price formatter and return to selection btn

--- frontend/src/components/organisms/atm/MyBankAccountDisplay.vue
+++ frontend/src/components/organisms/atm/MyBankAccountDisplay.vue
@@ -5,8 +5,10 @@
     import router from '@/router/router.js'
     import { useErrorHandlingStore } from '@/stores/errorHandlingStore.js'
     import { useAuthStore } from '@/stores/authStore.js'
+    import { getPriceFormatted } from '@/utils/stringFormatter.js'
 
     import ATMActionOptions from '@/components/molecules/atm/ATMActionOptions.vue'
+    import ReturnBtn from '@/components/atoms/buttons/ReturnBtn.vue'
     
     const errorHandlingStore = useErrorHandlingStore()
     const authStore = useAuthStore()
@@ -31,8 +33,9 @@
 </script>
 
 <template>
+    <ReturnBtn text="Return to bank account selection" to="/atm/select-bank-account" />
     <h3>Welcome, {{ authStore.atmDecodedAuthToken?.firstName }} </h3>
     <h3>{{ route.params.iban }} | {{ bankAccount?.bankAccountType }}</h3>
-    <h3>Balance: {{ bankAccount?.balance }} </h3>
+    <h3>Balance: {{ getPriceFormatted(bankAccount?.balance) }} </h3>
     <ATMActionOptions :iban="route.params.iban" />
 </template>
