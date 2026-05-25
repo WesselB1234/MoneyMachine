dd4b6d21ed57b3bab41bd8b9650b3871637b9903
WesselB1234 <stosin007@hotmail.com>
2026-05-25 18:37:53 +0200

    Feat: added atomic files to put the frontend stuff into

--- frontend/src/components/pages/atm/ATMMyBankAccount.vue
+++ frontend/src/components/pages/atm/ATMMyBankAccount.vue
@@ -1,33 +1,7 @@
 <script setup>
-    import { onMounted, ref } from 'vue'
-    import { useRoute } from 'vue-router'
-    import axios from '@/utils/axios.js'
-    import router from '@/router/router.js'
-    import { useErrorHandlingStore } from '@/stores/errorHandlingStore.js'
-    
-    const errorHandlingStore = useErrorHandlingStore()
-    const route = useRoute()
-    const bankAccount = ref(null)
-    
-    onMounted(async () => {
-        try {
-            const response = await axios.get('/bank-accounts/' + route.params.iban)
-            bankAccount.value = response.data
-
-            console.log(bankAccount.value)
-        }
-        catch (ex){
-            if (ex.response){
-                errorHandlingStore.errorMessage = ex.response.data.details
-                router.push('/atm/select-bank-account')
-            }
-            else {
-                useErrorHandlingStore.errorMessage = ex.details
-            }
-        }
-    })
+    import MyBankAccountDisplay from '@/components/organisms/atm/MyBankAccountDisplay.vue'
 </script>
 
 <template>
-    
+    <MyBankAccountDisplay />
 </template>
