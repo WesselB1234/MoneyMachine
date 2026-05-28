4aeb12cc3ce69034aef53e7a9d2c0fa0f5f346b1
WesselB1234 <stosin007@hotmail.com>
2026-05-28 23:50:55 +0200

    Test: adding a generated test from vsc and playing with it

---
+++ backend/src/test/java/MoneyMachine/services/AuthenticationServiceImplTest.java
@@ -0,0 +1,27 @@
+package MoneyMachine.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthenticationServiceImplTest {
+    @Test
+    void testGetHashedPassword() {
+        assertEquals(false, true);
+    }
+
+    @Test
+    void testGetLoggedInUser() {
+
+    }
+
+    @Test
+    void testGetLoggedInUserResponse() {
+
+    }
+
+    @Test
+    void testLogin() {
+
+    }
+}
