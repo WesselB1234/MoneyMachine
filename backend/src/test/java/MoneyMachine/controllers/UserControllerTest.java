830714b94c50b2ecd2a3354433046be115f7ad82
WesselB1234 <stosin007@hotmail.com>
2026-06-01 21:09:28 +0200

    Refactor: removing unused imports

--- backend/src/test/java/MoneyMachine/controllers/UserControllerTest.java
+++ backend/src/test/java/MoneyMachine/controllers/UserControllerTest.java
@@ -3,27 +3,16 @@ package MoneyMachine.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.http.MediaType;
 
 import tools.jackson.databind.ObjectMapper;
 
-import MoneyMachine.models.User;
-import MoneyMachine.models.enums.Role;
-import MoneyMachine.repositories.UserRepository;
-
-import static org.hamcrest.Matchers.hasItem;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
