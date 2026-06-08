8fcab93c2bb97ce7a2032ca7cc087e38e759253b
WesselB1234 <stosin007@hotmail.com>
2026-06-08 13:46:34 +0200

    Fix: removing duplicate bootstrap import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,6 @@ import router from './router/router'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App);
 app.use(createPinia())
